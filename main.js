682a37fb0cde5ae11b447bb8de8435fd977ff3ec
Yocson Wang <yocson@outlook.com>
2018-05-29 21:35:23 -0400

    package

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const electron = require('electron')
-const {app, BrowserWindow} = electron
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
 
-app.on('ready', ()=>{
-    let win = new BrowserWindow({width: 800, height: 600})
-    win.loadURL("https://fanfou.com");
-})
+
+function createWindow () {
+    win = new BrowserWindow({width: 800, height: 600, title: "大锅饭"})
+  
+    win.loadURL('https://fanfou.com')
+  
+    win.on('closed', () => {
+      win = null
+    })
+  }
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+app.on('activate', () => {
+    if (win === null) {
+      createWindow()
+    }
+  })
